b37cd058c5a9163675034730a50e8bb0e6123437
Csaba Maulis <hello@senki.xyz>
2021-03-13 19:06:58 +0800

    Build app in one step

--- create-app-bundle.sh
+++ create-app-bundle.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Building AutoRaise.app" && \
+rm -r AutoRaise && \
 rm -rf AutoRaise.app && \
+g++ -O2 -Wall -fobjc-arc -o AutoRaise AutoRaise.mm -framework AppKit && \
 mkdir -p AutoRaise.app/Contents/MacOS && \
 mkdir AutoRaise.app/Contents/Resources && \
 cp AutoRaise AutoRaise.app/Contents/MacOS && \
 cp Info.plist AutoRaise.app/Contents && \
 cp AutoRaise.icns AutoRaise.app/Contents/Resources && \
-chmod 700 AutoRaise.app && echo "Successfully created AutoRaise.app"
+chmod 700 AutoRaise.app && \
+echo "Successfully created AutoRaise.app"
